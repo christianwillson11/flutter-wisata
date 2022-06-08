b00c59490df0ca11cc022f7b2e9f509ff4b09857
RichardoJ <79075790+RichardoJ@users.noreply.github.com>
2022-06-08 20:58:22 +0700

    Tambah Weather (Belum dikonekkan)

---
+++ lib/pages/apiservices.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter_application_1/weatherData.dart';
+import 'package:http/http.dart' as http;
+
+class service{
+  Future<Weather> getWeatherData(String location) async {
+    Map<String, String> requestHeaders = {
+      "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+	    "X-RapidAPI-Key": "2bf8da321dmsh17ce66024ac8067p1c5c94jsn7698a8baa3c0"
+     };
+    
+    final response = await http.get(
+      Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$location&appid=d881571f5c62312c8f5d0ffbada6beee'),
+      headers: requestHeaders
+    );
+
+    if(response.statusCode == 200){
+      Weather jsonResponse = json.decode(response.body);
+      print(jsonResponse.cityName);
+      return jsonResponse;
+      // List jsonResponse = json.decode(response.body);
+      // return jsonResponse.map((data) => Weather.fromJson(data)).toList();
+    }else{
+      throw('Failed to load Data');
+    }
+
+    print(response.body.toString());
+  }
+}
