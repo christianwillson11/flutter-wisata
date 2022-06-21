29fcb95d8cafe819f183189c6278579528cce88e
RichardoJ <79075790+RichardoJ@users.noreply.github.com>
2022-06-21 21:52:56 +0700

    update detailhotel.dart (foto)

--- lib/pages/search page/detailhotel.dart
+++ lib/pages/search page/detailhotel.dart
@@ -15,97 +15,100 @@ class _detailHotelState extends State<detailHotel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Container(
-        child: Column(
-          children: [
-            Container(
-              child: Image(
-                image: NetworkImage(
-                    widget.myhotel.gambar.toString()),
-                fit: BoxFit.cover,
+      body: SingleChildScrollView(
+      child: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 230,
+                child: Image(
+                  image: NetworkImage(
+                      widget.myhotel.gambar.toString()),
+                  fit: BoxFit.fill,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 12,
-                right: 24,
-                top: 16,
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 12,
+                  right: 24,
+                  top: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // SizedBox(
+                    //   height: 8,
+                    // ),
+                    Text(
+                      widget.myhotel.hotelName.toString(),
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          widget.myhotel.locality.toString(),
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        SizedBox(
+                          width: 32,
+                        ),
+                        Text(
+                          "Rating : ",
+                          style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontStyle: FontStyle.italic),
+                        ),
+                        Text(
+                          widget.myhotel.rating.toString(),
+                          style: TextStyle(
+                              color: Colors.blueGrey,
+                              fontStyle: FontStyle.italic),
+                        ),
+                        SizedBox(width: 4,),
+                        Icon(
+                          Icons.star,
+                          color: Colors.greenAccent,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    Text(
+                      widget.myhotel.alamat.toString(),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Text(
+                      "User Story",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // Container(
+                    //   child: Text("Ini Test Scroll view"),
+                    //   height: 150.0,
+                    //   decoration: BoxDecoration(
+                    //     color: Colors.red
+                    //   ),
+                    // ),
+                  ],
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // SizedBox(
-                  //   height: 8,
-                  // ),
-                  Text(
-                    widget.myhotel.hotelName.toString(),
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 18,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        widget.myhotel.locality.toString(),
-                        style: TextStyle(fontSize: 14),
-                      ),
-                      SizedBox(
-                        width: 32,
-                      ),
-                      Text(
-                        "Rating : ",
-                        style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontStyle: FontStyle.italic),
-                      ),
-                      Text(
-                        widget.myhotel.rating.toString(),
-                        style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontStyle: FontStyle.italic),
-                      ),
-                      SizedBox(width: 4,),
-                      Icon(
-                        Icons.star,
-                        color: Colors.greenAccent,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 14,
-                  ),
-                  Text(
-                    widget.myhotel.alamat.toString(),
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.blueGrey,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Text(
-                    "User Story",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  // Container(
-                  //   child: Text("Ini Test Scroll view"),
-                  //   height: 150.0,
-                  //   decoration: BoxDecoration(
-                  //     color: Colors.red
-                  //   ),
-                  // ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     ));
