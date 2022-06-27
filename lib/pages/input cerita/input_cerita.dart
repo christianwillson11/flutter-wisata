8b5997db23bbc1992fefa16dd3ce4e3ddfada1e8
Christian Willson <79264788+christianwillson11@users.noreply.github.com>
2022-06-27 09:40:04 +0700

    UI fix

--- lib/pages/input cerita/input_cerita.dart
+++ lib/pages/input cerita/input_cerita.dart
@@ -13,7 +13,12 @@ class InputCerita extends StatefulWidget {
   final String idCity;
   final String idContext;
   final String konteks;
-  const InputCerita({Key? key, required this.idCity, required this.idContext, required this.konteks}) : super(key: key);
+  const InputCerita(
+      {Key? key,
+      required this.idCity,
+      required this.idContext,
+      required this.konteks})
+      : super(key: key);
 
   @override
   State<InputCerita> createState() => _InputCeritaState();
@@ -37,13 +42,14 @@ class _InputCeritaState extends State<InputCerita> {
   bool _isUploading = false;
 
   void uploadFunction(List<XFile> _images, StoriesItem dt) async {
-
     if (_images.isNotEmpty) {
       setState(() {
         _isUploading = true;
       });
       if (dt.judulCerita == "" || dt.isiCerita == "") {
-        Fluttertoast.showToast(msg: "Gagal Menambahkan cerita. Harap lengkapi data", toastLength: Toast.LENGTH_LONG);
+        Fluttertoast.showToast(
+            msg: "Gagal Menambahkan cerita. Harap lengkapi data",
+            toastLength: Toast.LENGTH_LONG);
       } else {
         for (int i = 0; i < _images.length; i++) {
           var imageUrl = await Database.uploadFile(image: _images[i]);
@@ -57,15 +63,15 @@ class _InputCeritaState extends State<InputCerita> {
         setState(() {
           Navigator.pop(context);
         });
-      } 
+      }
     } else {
-      Fluttertoast.showToast(msg: "Anda harus menyertakan setidaknya 1 foto!", toastLength: Toast.LENGTH_LONG);
+      Fluttertoast.showToast(
+          msg: "Anda harus menyertakan setidaknya 1 foto!",
+          toastLength: Toast.LENGTH_LONG);
     }
-    
   }
 
   Future<void> selectImage() async {
-    
     if (_selectedFiles != null) {
       _selectedFiles.clear();
     }
@@ -79,24 +85,37 @@ class _InputCeritaState extends State<InputCerita> {
       Fluttertoast.showToast(msg: e.toString(), toastLength: Toast.LENGTH_LONG);
     }
 
-    setState(() {
-      
-    });
-
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.idContext),
-        ),
         body: SingleChildScrollView(
           child: Stack(
             children: [
+              ClipPath(
+                clipper: CurvedBottomClipper(),
+                child: Container(
+                  color: Colors.lightGreen,
+                  height: 250.0,
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 50),
+                      child: Text(
+                        "Bagikan ceritamu",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
               Container(
-                padding: EdgeInsets.fromLTRB(8, 50, 8, 8),
+                padding: EdgeInsets.fromLTRB(8, MediaQuery.of(context).size.height / 4, 8, 8),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -156,28 +175,25 @@ class _InputCeritaState extends State<InputCerita> {
                               }),
                         ),
                       ),
-
                     (_isUploading == false)
-                    ? ElevatedButton(
-                      child: const Text('Submit'),
-                      onPressed: () {
-                        final dt = StoriesItem(
-                          cityId: widget.idCity,
-                          locationId: widget.idContext,
-                          judulCerita: _judulCerita.text.toString(),
-                          isiCerita: _isiCerita.text.toString(),
-                          image: _arrImageUrl,
-                          owner: FirebaseAuth.instance.currentUser!.uid,
-                          category: widget.konteks
-                          );
-                        uploadFunction(_selectedFiles, dt);
-                      },
-                    )
-                    : const SizedBox(
-                        width: 15,
-                        height: 15,
-                        child: CircularProgressIndicator()
-                      ),
+                        ? ElevatedButton(
+                            child: const Text('Submit'),
+                            onPressed: () {
+                              final dt = StoriesItem(
+                                  cityId: widget.idCity,
+                                  locationId: widget.idContext,
+                                  judulCerita: _judulCerita.text.toString(),
+                                  isiCerita: _isiCerita.text.toString(),
+                                  image: _arrImageUrl,
+                                  owner: FirebaseAuth.instance.currentUser!.uid,
+                                  category: widget.konteks);
+                              uploadFunction(_selectedFiles, dt);
+                            },
+                          )
+                        : const SizedBox(
+                            width: 15,
+                            height: 15,
+                            child: CircularProgressIndicator()),
                   ],
                 ),
               ),
@@ -186,5 +202,43 @@ class _InputCeritaState extends State<InputCerita> {
         ),
       ),
     );
+  }
+}
+
+class CurvedBottomClipper extends CustomClipper<Path> {
+  var pi = 22 / 7;
+  @override
+  Path getClip(Size size) {
+    // I've taken approximate height of curved part of view
+    // Change it if you have exact spec for it
+    final roundingHeight = size.height * 3 / 8;
+
+    // this is top part of path, rectangle without any rounding
+    final filledRectangle =
+        Rect.fromLTRB(0, 0, size.width, size.height - roundingHeight);
+
+    // this is rectangle that will be used to draw arc
+    // arc is drawn from center of this rectangle, so it's height has to be twice roundingHeight
+    // also I made it to go 5 units out of screen on left and right, so curve will have some incline there
+    final roundingRectangle = Rect.fromLTRB(
+        -5, size.height - roundingHeight * 2, size.width + 5, size.height);
+
+    final path = Path();
+    path.addRect(filledRectangle);
+
+    // so as I wrote before: arc is drawn from center of roundingRectangle
+    // 2nd and 3rd arguments are angles from center to arc start and end points
+    // 4th argument is set to true to move path to rectangle center, so we don't have to move it manually
+    path.arcTo(roundingRectangle, pi, -pi, true);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // returning fixed 'true' value here for simplicity, it's not the part of actual question, please read docs if you want to dig into it
+    // basically that means that clipping will be redrawn on any changes
+    return true;
   }
 }
